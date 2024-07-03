6936e54177a98a18bb72703b71386f5da175c82c
Lucas Yuji <160159812+LucaYuji@users.noreply.github.com>
2024-07-03 18:17:03 -0300

    Add files via upload

---
+++ do_while.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int cont;
+
+    cont = 0;
+    do{ // faça depois verifique
+        cout << "hello world = " << cont << "\n"; 
+        cont++;
+    }while(cont < 20); //verifica o teste 
+
+    cout << "\nRotina Finalizada\n";
+
+    return 0;
+}
